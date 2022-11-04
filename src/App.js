c27e2193c67e457a508e40f06498fa2db7c95b43
Mariia IVANCHENKO <mariiaivanchenko@mbp-de-mariia.home>
2022-11-04 22:45:06 +0100

    final

--- src/App.js
+++ src/App.js
@@ -17,52 +17,81 @@ const App = () => {
       </header>
       <main className="main">
         <div className="ajoutBtn">
-          <button
-            className="counterPlus"
-            onClick={() => {
-              const newCounter = [...counter];
-              newCounter.push(0);
-              setCounter(newCounter);
-            }}
-          >
-            Add counter
-          </button>
+          {counter.length >= 3 ? (
+            <button
+              className="counterPlus"
+              onClick={() => {
+                setCounter([0]);
+              }}
+            >
+              Close counter
+            </button>
+          ) : (
+            <button
+              className="counterPlus"
+              onClick={() => {
+                if (counter.length < 3) {
+                  const newCounter = [...counter];
+                  newCounter.push(0);
+                  setCounter(newCounter);
+                }
+              }}
+            >
+              Add counter
+            </button>
+          )}
         </div>
-
-        {counter.map((elem, index) => {
-          return (
-            <div className="container" key={index}>
-              <div className="elemHaut">
-                <img
-                  src={Moins}
-                  alt="moins"
-                  onClick={() => {
-                    setCounter(counter[index] - 1);
-                  }}
-                />
-                <span className="counter">{elem}</span>
-                <img
-                  src={Plus}
-                  alt=" plus"
-                  onClick={() => {
-                    setCounter(counter[index] + 1);
-                  }}
-                />
+        <div className="newCounter">
+          {counter.map((elem, index) => {
+            return (
+              <div className="container">
+                <div className="elemHaut">
+                  <img
+                    src={Moins}
+                    alt="moins"
+                    onClick={() => {
+                      const newCounter = [...counter];
+                      newCounter[index] = counter[index] - 1;
+                      setCounter(newCounter);
+                    }}
+                    style={{ visibility: elem === 0 ? "hidden" : "visible" }}
+                  />
+                  <span className="counter">{elem}</span>
+                  <img
+                    src={Plus}
+                    alt=" plus"
+                    onClick={() => {
+                      const newCounter = [...counter];
+                      newCounter[index] = counter[index] + 1;
+                      setCounter(newCounter);
+                    }}
+                    style={{ visibility: elem === 10 ? "hidden" : "visible" }}
+                  />
+                </div>
+                <div className="elemBas">
+                  <button
+                    className="reset"
+                    onClick={() => {
+                      const newCounter = [...counter];
+                      newCounter[index] = 0;
+                      setCounter(newCounter);
+                    }}
+                  >
+                    Reset
+                  </button>
+                </div>
               </div>
-              <div className="elemBas">
-                <button
-                  className="reset"
-                  onClick={() => {
-                    setCounter(counter[index]);
-                  }}
-                >
-                  Reset
-                </button>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </main>
+
+      <footer>
+        <p>
+          Made by <span>React</span> avec <span>Le Reacteur</span> by
+          <span> Mariia MERCIER</span>
+        </p>
+      </footer>
     </div>
   );
 };
