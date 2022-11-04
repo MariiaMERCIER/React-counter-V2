568f0e14e947251c7121a9c595080d00fec08ee6
Mariia IVANCHENKO <mariiaivanchenko@mbp-de-mariia.home>
2022-11-04 17:14:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Counter from "./assets/images/counter.png";
+import Moins from "./assets/images/moins.png";
+import Plus from "./assets/images/plus.png";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState([0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div className="header">
+          <img src={Counter} alt="logo" />
+          <h1>React Counter V2</h1>
+        </div>
       </header>
+      <main className="main">
+        <div className="ajoutBtn">
+          <button
+            className="counterPlus"
+            onClick={() => {
+              const newCounter = [...counter];
+              newCounter.push(0);
+              setCounter(newCounter);
+            }}
+          >
+            Add counter
+          </button>
+        </div>
+
+        {counter.map((elem, index) => {
+          return (
+            <div className="container" key={index}>
+              <div className="elemHaut">
+                <img
+                  src={Moins}
+                  alt="moins"
+                  onClick={() => {
+                    setCounter(counter[index] - 1);
+                  }}
+                />
+                <span className="counter">{elem}</span>
+                <img
+                  src={Plus}
+                  alt=" plus"
+                  onClick={() => {
+                    setCounter(counter[index] + 1);
+                  }}
+                />
+              </div>
+              <div className="elemBas">
+                <button
+                  className="reset"
+                  onClick={() => {
+                    setCounter(counter[index]);
+                  }}
+                >
+                  Reset
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </main>
     </div>
   );
-}
+};
 
 export default App;
